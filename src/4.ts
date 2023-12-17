3d717a003fd176a120126e61a4ce53dbc82b8008
Bragi1989 <130233226+Bragi1989@users.noreply.github.com>
2023-12-17 13:17:27 +0100

    1

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,60 @@
-const key = new Key();
+class Key {
+  private signature: number
 
-const house = new MyHouse(key);
-const person = new Person(key);
+  constructor () {
+    this.signature = Math.random()
+  }
+  getSignature () {
+    return this.signature
+  }
+}
 
-house.openDoor(person.getKey());
+class Person {
+  private key: Key
+  constructor (key: Key) {
+    this.key = key
+  }
 
-house.comeIn(person);
+  getKey () {
+    return this.key
+  }
+}
 
+abstract class House {
+  door: boolean = false
+  key: Key
+  tenants: Person[] = []
 
-export {};
+  comeIn (person: Person) {
+    if (this.door) {
+      return this.tenants.push(person)
+    }
+  }
+
+  public abstract openDoor(key: Key)
+}
+
+class MyHouse extends House {
+  constructor (key: Key) {
+    super()
+    this.key = key
+  }
+  public openDoor (key: Key) {
+    if (key.getSignature() === this.key.getSignature()) {
+      return (this.door = true)
+    } else {
+      return (this.door = false)
+    }
+  }
+}
+
+const key = new Key()
+const house = new MyHouse(key)
+const person = new Person(key)
+house.openDoor(person.getKey())
+
+house.comeIn(person)
+
+console.log(house.tenants)
+
+export {}
